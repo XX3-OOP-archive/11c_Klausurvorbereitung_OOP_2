49cda341d2f44c9ce3e7281bcb80c62ae999aa05
Peter <schmidt.multimedia@gmail.com>
2021-12-07 10:56:02 +0100

    update Themen

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -4,6 +4,15 @@ public class App {
     
     public static void main(String[] args) {
 
+
+     Test test = new Test("Grizabella", 29);
+
+     System.out.println(test.getName());
+
+
+
+
+
     
     }
 
---
+++ src/com/cc/java/Test.java
@@ -0,0 +1,40 @@
+package com.cc.java;
+
+public class Test {
+    
+    //Felder (fields) | Attribute, Eigenschaften
+    // private --> Kapselung / encapsulation/ data hiding
+
+    private String name;
+    private int age;
+
+    public Test(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+	// Sichtbarkeit | access (access modifiers)
+	// public --> alle dürfen sehen/schreiben (+)
+	// private --> NUR die eigene Klasse / Objekt (-)
+	// protected --> NUR die eigene Klasse / Objekt + Subklassen (#)
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+}
